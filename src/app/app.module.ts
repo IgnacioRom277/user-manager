5e66aa7b2a56e733c3447db93d4bb6f5532fc49d
unknown <ignaciorom27@gmail.com>
2021-06-05 20:55:56 -0500

    Home and ErrorPage components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LocalStorageService } from './services/utils/local-storage.service';
 import { LoginComponent } from './pages/login/login.component';
 import { SharedModule } from './modules/shared/shared.module';
@@ -11,6 +13,8 @@ import { SharedModule } from './modules/shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
+    ErrorPageComponent,
+    HomeComponent,
     LoginComponent
   ],
   imports: [
