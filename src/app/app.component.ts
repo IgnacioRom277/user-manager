47a2d4d5b3691e900b76c4635d1ce31c4ac5006c
unknown <ignaciorom27@gmail.com>
2021-06-06 18:55:29 -0500

    Toolbar and sidebar implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from './services/utils/local-storage.service';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'user-manager';
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+  public isExpanded = true;
+
+  constructor(
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) {}
+
+  onLogout() {
+    this.localStorage.removeItem('token');
+    this.router.navigate(['/']).then(() => {
+      window.location.reload();
+    });
+  }
 }
