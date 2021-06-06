0140923ea5ff725509cde2e2fcc291ad4224dac0
unknown <ignaciorom27@gmail.com>
2021-06-06 01:37:53 -0500

    Home page with users request created

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+
+import { PaginatorService } from './../../services/utils/paginator-service';
+import { User, UserReponse } from './../../interfaces/users';
+import { UserService } from './../../services/users/users.service';
+
+const DISPLAYED_COLUMNS = ['preview', 'email', 'first_name', 'last_name'];
 
 @Component({
   selector: 'app-home',
@@ -6,10 +14,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public dataSource!: MatTableDataSource<User>;
+  public displayedColumns: string[] = DISPLAYED_COLUMNS;
+  public totalUsers!: number;
+  public userList!: Array<User>;
+  public userResponse!: UserReponse;
+  public usersPerPage!: number;
 
-  constructor() { }
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+
+  constructor(
+    private userService: UserService,
+    private paginatorService: PaginatorService
+  ) { }
 
   ngOnInit(): void {
+    this.getUsers();
+    this.dataSource = new MatTableDataSource();
+    this.paginatorService.paginatorLabels(this.paginator);
   }
 
+  /**
+   * @description Get users data according to index set
+   * @param {PageEvent} event Optional event when page in paginator change
+   */
+  public async getUsers(event?: PageEvent): Promise<void> {
+    const index = event ? event.pageIndex + 1 : 1;
+    this.userResponse = await this.userService.getUsers(index);
+    if (this.userResponse && this.userResponse.data.length) {
+      this.userList = this.userResponse.data;
+      this.dataSource = new MatTableDataSource(this.userResponse.data);
+      this.totalUsers = this.userResponse.total;
+      this.usersPerPage = this.userResponse.per_page;
+    }
+  }
 }
