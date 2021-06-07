2e170444254b05ae19b56507948135a259b60554
unknown <ignaciorom27@gmail.com>
2021-06-06 19:26:07 -0500

    Toolbar navigation added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,62 @@
 import { Router } from '@angular/router';
-import { LocalStorageService } from './services/utils/local-storage.service';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+
+import { LocalStorageService } from './services/utils/local-storage.service';
+import { AuthService } from '../app/services/authentication/auth.service';
+import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'user-manager';
+export class AppComponent implements OnInit {
   @ViewChild('sidenav') sidenav!: MatSidenav;
   public isExpanded = true;
+  public isUserLoggedIn: boolean = false;
+  public subscription!: Subscription;
+  public title: string = 'user-manager';
 
   constructor(
+    private authService: AuthService,
     private localStorage: LocalStorageService,
+    private location: Location,
     private router: Router
   ) {}
 
-  onLogout() {
+  ngOnInit() {
+    this.subscription = this.authService.getLoggedIn().subscribe(value => {
+      this.isUserLoggedIn = value;
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  /**
+   * @description Remove token at local storage
+   */
+  public onLogout(): void {
     this.localStorage.removeItem('token');
-    this.router.navigate(['/']).then(() => {
-      window.location.reload();
-    });
+    window.location.reload();
+  }
+
+  /**
+   * @description Returns to last location
+   */
+  public onBack(): void {
+    this.location.back();
+  }
+
+  /**
+   * @description Go to home url and reload page
+   */
+  public onHome(): void {
+    this.router.navigate(['/home']);
   }
 }
